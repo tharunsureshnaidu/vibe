cae045d303c2d98c83f028f650694eafe2aeb850
tharunsureshnaidu <tharunsureshsjtksjtksjtk@gmail.com>
2025-09-08 12:24:43 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+
+
+const home= () =>{
+  return (
+    <div>
+    <div className="font-bold text-rose-500">
+      hello
+      
+    </div>
+    <Button variant="new" className="font-bold">
+      click me
+    </Button>
+    </div>
+  );
+
+}
+export default home;
