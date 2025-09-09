0df20ae3ba02480bd12f2b2618504da73c7f135f
tharunsureshnaidu <tharunsureshsjtksjtksjtk@gmail.com>
2025-09-09 18:15:05 +0530

    e2bsandbx

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,7 @@
 import { inngest } from "./client";
 import Groq from "groq-sdk";
-
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export const helloWorld = inngest.createFunction(
@@ -8,6 +9,10 @@ export const helloWorld = inngest.createFunction(
   { event: "test/hello.world" },
   async ({ event, step }) => {
     // Define Groq LLM prompt and system message
+    const sandboxId=await step.run("get-sandbox-id",async ()=>{
+      const sandbox = await Sandbox.create("vibe-test-next3");
+      return sandbox.sandboxId
+    })
     const prompt = `create the components for the following: ${event.data.value}`;
     const system_message = "You are an expert in coding in nextjs ";
 
@@ -21,7 +26,13 @@ export const helloWorld = inngest.createFunction(
     });
 
     const output = choices?.[0].message?.content || "";
+
+    const sandboxurl= await step.run("get-sandbox-url",async ()=>{
+      const sandbox=await getSandbox(sandboxId)
+      const host=sandbox.getHost(3000)
+      return `https://${host}`
+    })
     console.log(output);
-    return { output };
+    return { output ,sandboxurl};
   }
 );
---
+++ src/inngest/utils.ts
@@ -0,0 +1,5 @@
+import {Sandbox} from "@e2b/code-interpreter"
+export async function getSandbox(sandboxId:string){
+    const sandbox = await Sandbox.connect(sandboxId)
+    return sandbox
+}
