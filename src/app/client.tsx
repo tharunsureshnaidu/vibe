67aaa6ae9f3a975f0b05ba8fff59a393782770c7
tharunsureshnaidu <tharunsureshsjtksjtksjtk@gmail.com>
2025-09-09 10:39:42 +0530

    added Inngest

---
+++ src/app/client.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+
+export const Client =() =>{
+    const trpc=useTRPC()
+    const {data} = useSuspenseQuery(trpc.hello.queryOptions({text:"Tharun"}))
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
+// import { getQueryClient,trpc } from "@/trpc/server";  
+// import { dehydrate,HydrationBoundary } from "@tanstack/react-query";
+// import { Client } from "./client";
+// import { Suspense } from "react";
 
-const home= async() =>{
+// const page = async () =>{
+//   const queryClient = getQueryClient()
+//   void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"Tharun"}))
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <Suspense fallback={<p>loading...</p>}>
+//         <Client />
+//       </Suspense>
+      
+//     </HydrationBoundary>
+//   )
+// }
+// export default page
 
-  return (
-    <div>piss oddd
-   
+"use client"
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query"
+import { toast } from "sonner"
+const page=()=>{
+  const trpc=useTRPC()
+  const invoke=useMutation(trpc.invoke.mutationOptions({
+    onSuccess:()=>{
+      toast.success("Background job started")
+    }
+  }))
+  return(
+    <div>
+      test
+      <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"John"})}>
+        invoke background job
+      </Button>
+
     </div>
-  );
-
+  )
 }
-export default home;
+export default page
